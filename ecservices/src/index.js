9764f24d52bfe77b496bdf8a79c1e032fe7f7d1a
andleebrehman <andleebrehman36@gmail.com>
2026-05-05 14:34:32 +0500

    fix issue

--- ecservices/src/index.js
+++ ecservices/src/index.js
@@ -6,8 +6,10 @@ import {
 import App from "./App";
 import "./index.css";
 
-ReactDOM.createRoot(document.getElementById("root")).render(
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>
+ReactDOM.createRoot(document.getElementById("root")).render( <
+    BrowserRouter >
+    <
+    App / >
+    <
+    /BrowserRouter>
 );
